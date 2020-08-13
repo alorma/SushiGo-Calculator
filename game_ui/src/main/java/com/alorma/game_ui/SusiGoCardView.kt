2a943defabe9122cee35bf989cf235185527d86e
Bernat Borrás <bernat.borras@adevinta.com>
2020-08-13 08:23:30 +0200

    Rounded corners

--- game_ui/src/main/java/com/alorma/game_ui/SusiGoCardView.kt
+++ game_ui/src/main/java/com/alorma/game_ui/SusiGoCardView.kt
@@ -1,15 +1,12 @@
 package com.alorma.game_ui
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.ColorStateList
-import android.content.res.TypedArray
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
-import androidx.core.content.withStyledAttributes
-import com.google.android.material.card.MaterialCardView
 import com.google.android.material.color.MaterialColors
 import com.google.android.material.shape.MaterialShapeDrawable
 import com.google.android.material.shape.ShapeAppearanceModel
@@ -18,56 +15,49 @@ import java.util.*
 
 class SusiGoCardView @JvmOverloads constructor(
     context: Context,
-    attributeSet: AttributeSet? = null,
+    attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
     defStyleRes: Int = 0
-) : MaterialCardView(context, attributeSet, defStyleAttr) {
+) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    @SuppressLint("PrivateResource")
-    private var tableCardElevation = context.resources.getDimension(R.dimen.mtrl_card_elevation)
+    private val mediumShape = ShapeAppearanceModel.builder(
+        context,
+        attrs,
+        R.attr.shapeAppearanceMediumComponent,
+        R.style.ShapeAppearance_MaterialComponents_MediumComponent
+    ).build()
+
+    private val tableBackground = MaterialShapeDrawable(
+        context,
+        attrs,
+        defStyleAttr,
+        defStyleRes
+    )
 
     init {
         LayoutInflater.from(context).inflate(R.layout.sushi_go_card, this, true)
-
-        context.withStyledAttributes(
-            attributeSet,
-            com.google.android.material.R.styleable.CardView,
-            defStyleAttr,
-            defStyleRes
-        ) {
-            readProperties()
-            sushiGoCardTable.background = createTableDrawable()
-        }
+        sushiGoCardTable.background = createTableDrawable()
     }
 
-    private fun createTableDrawable(): Drawable =
-        MaterialShapeDrawable.createWithElevationOverlay(context).apply {
-            elevation = tableCardElevation
-
-            val color = context.getMergedColor(
-                overlayColor = MaterialColors.getColor(this@SusiGoCardView, R.attr.colorGameTable),
-                overlayAlpha = 0.5f
-            )
-            fillColor = ColorStateList.valueOf(color)
-            strokeWidth = 0f
-
-            val cardShape = this@SusiGoCardView.shapeAppearanceModel
-            val tableShape = ShapeAppearanceModel.builder()
-                .setBottomLeftCorner(cardShape.bottomLeftCorner)
-                .setBottomLeftCornerSize(cardShape.bottomLeftCornerSize)
-                .setBottomEdge(cardShape.bottomEdge)
-                .setBottomRightCornerSize(cardShape.bottomRightCornerSize)
-                .setBottomRightCorner(cardShape.bottomRightCorner)
-                .build()
-            shapeAppearanceModel = tableShape
-        }
-
-    @SuppressLint("PrivateResource")
-    private fun TypedArray.readProperties() {
-        tableCardElevation = getDimension(
-            com.google.android.material.R.styleable.CardView_cardElevation,
-            tableCardElevation
+    private fun createTableDrawable(): Drawable {
+        tableBackground.initializeElevationOverlay(context)
+        val color = context.getMergedColor(
+            overlayColor = MaterialColors.getColor(this@SusiGoCardView, R.attr.colorGameTable),
+            overlayAlpha = 0.5f
         )
+        tableBackground.fillColor = ColorStateList.valueOf(color)
+
+        val tableShape = ShapeAppearanceModel.builder()
+            .setBottomLeftCorner(mediumShape.bottomLeftCorner)
+            .setBottomLeftCornerSize(mediumShape.bottomLeftCornerSize)
+            .setBottomEdge(mediumShape.bottomEdge)
+            .setBottomRightCornerSize(mediumShape.bottomRightCornerSize)
+            .setBottomRightCorner(mediumShape.bottomRightCorner)
+            .build()
+        tableBackground.shapeAppearanceModel = tableShape
+
+
+        return tableBackground
     }
 
     fun setCard(sushiGoCard: SushiGoUiCard) {
@@ -75,7 +65,7 @@ class SusiGoCardView @JvmOverloads constructor(
             overlayColor = ContextCompat.getColor(context, sushiGoCard.color),
             overlayAlpha = 0.6f
         )
-        setCardBackgroundColor(color)
+        setBackgroundColor(color)
 
         val textColor = context.getMergedColor(
             overlayColor = ContextCompat.getColor(context, sushiGoCard.color),
